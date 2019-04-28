11923ca25eb1cdcbb4975b20834739d726070c52
Christian Bedwell <32681621+ChristianBedwell@users.noreply.github.com>
2019-04-28 11:38:08 -0400

    Delete AccountOverdrawnException.java

--- src/CEN 4072 - Software Testing/AccountOverdrawnException.java
+++
@@ -1,9 +0,0 @@
-// user defined exception for account overdrawn
-public class AccountOverdrawnException extends Exception {
-
-	private static final long serialVersionUID = 3073893446951649972L;
-
-	public AccountOverdrawnException() {
-		super("Account cannot be less than $50.00");
-	}	
-}
