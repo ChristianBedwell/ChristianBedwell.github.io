7ec68b36bbae834b6412730604412067b71de20e
Christian Bedwell <32681621+ChristianBedwell@users.noreply.github.com>
2019-04-28 11:38:30 -0400

    Delete InsufficientFundsException.java

--- src/CEN 4072 - Software Testing/InsufficientFundsException.java
+++
@@ -1,9 +0,0 @@
-// user defined exception for insufficient funds in account
-public class InsufficientFundsException extends Exception {
-
-	private static final long serialVersionUID = -6667742746659274665L;
-
-	public InsufficientFundsException() {
-		super("Insufficient funds to complete transaction");
-	}	
-}
